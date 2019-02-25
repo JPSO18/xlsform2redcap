9124f946c09d501f586b503ffec997f4cee55519
Joao Paulo Oliveira <joao.paulojp18@hotmail.com>
2019-02-25 17:49:58 -0300

    Added data import helper

--- lib/redcap.ex
+++ lib/redcap.ex
@@ -14,6 +14,64 @@ defmodule Redcap do
 
     """
 
+    def migrate_data_odk_to_redcap(odk_data_path, data_guideline_path, event \\ nil) do
+      data_guide_map = csv_mapper(data_guideline_path, :data_guideline)
+      # mapped_data = csv_mapper(odk_data_path, :data)
+
+      stream =
+        odk_data_path
+        |> File.stream!()
+        |> CSV.decode!(headers: false)
+
+      body = stream |> Enum.map(fn n -> n end) |> List.delete_at(0)
+
+      headers =
+        stream
+        |> Enum.take(1)
+        |> List.flatten()
+        |> Enum.map(fn n ->
+          if data_guide_map[:"#{n}"] do
+            data_guide_map[:"#{n}"]
+          else
+            n
+          end
+        end)
+
+        file = File.open!("importtool.csv", [:write, :utf8, :append])
+
+        [headers] ++ body
+        |> CSV.encode()
+        |> Enum.each(&IO.write(file, &1))
+    end
+
+    defp csv_mapper(nil, atom), do: nil
+    defp csv_mapper('', atom), do: nil
+    defp csv_mapper(data_guideline_path, atom) do
+      data_guideline_path
+      |> File.stream!()
+      |> CSV.decode!()
+      |> Enum.reduce(%{}, &handle_mapping(&1, &2, atom))
+    end
+    # 'p1' stands for parameter 1, and so on
+    defp handle_mapping(p1, p2, :data_guideline), do: map_guideline(p1, p2)
+    # defp handle_mapping(p1, p2, :data), do: map_form_data(p1, p2)
+
+    defp map_guideline([col1, col2, col3], acc) do
+      [{:"#{col1}", col2}]
+      |> Map.new()
+      |> Map.merge(acc)
+    end
+
+    def map_form_data(cols, acc) do
+      if acc == %{} do
+        header_map = Enum.map(cols, fn col -> {:"#{col}", []})
+        acc = Map.merge(header_map, acc)
+      else
+        Enum.map(cols)
+
+      end
+    end
+
     def xlsx_to_redcap(path) when path |> is_binary(), do: do_parse(Xlsxir.multi_extract(path))
 
     defp do_parse({:ok, worksheet}) do
